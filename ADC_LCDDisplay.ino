0df000b9b51d5923219e399e6d6dc5256ed5426b
Junichi Motohisa <motohisa@ist.hokudai.ac.jp>
2017-06-03 08:01:44 +0900

    to work with LTC2450 16bit ADC

--- ADC_LCDDisplay.ino
+++ ADC_LCDDisplay.ino
@@ -14,33 +14,49 @@
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
 
- Read analog pin A5
- and convert to pressure (in Torr) for Varian multiguage
+ // Read analog pin A5
+ // and convert to pressure (in Torr) for Varian multiguage
 */
 
 // include the library code:
 #include <LiquidCrystal.h>
+#include <SPI.h>
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-int sensorPin = A5; // input pin
+// int sensorPin = A5; // input pin
 float vout = 0;
+unsigned char m_data; //MSB byte
+unsigned char l_data; //LSB byte
+const int CS = 10;
 
 void setup() {
   // put your setup code here, to run once:
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
 //  lcd.print("ADC value from A5");
-  analogReference(DEFAULT);
- }
+
+// ADC
+//  analogReference(DEFAULT);
+  pinMode(CS,OUTPUT);
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setDataMode(SPI_MODE3);
+  SPI.setClockDivider(SPI_CLOCK_DIV16);
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
   int dig;
   float exponent;
-  vout = analogRead(sensorPin)*4.883*0.001*2
-  ;
+//  vout = analogRead(sensorPin)*4.883*0.001*2
+  digitalWrite(CS,LOW);
+  m_data = SPI.transfer(0x00);
+  l_data = SPI.transfer(0x00);
+  digitalWrite(CS,HIGH);
+  vout=(m_data*256+l_data)*4.883*0.001*2;
+  
   lcd.setCursor(0,0);
   lcd.print("output=");
   lcd.setCursor(7,0);
