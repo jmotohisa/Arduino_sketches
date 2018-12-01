6ff3417a3bfad630f004ec208750f09c597512bc
Junichi Motohisa <motohisa@ist.hokudai.ac.jp>
2018-12-01 17:12:43 +0900

    GPSLogger4 updated

--- GPSLogger4/GPSLogger4/GPSLogger4.ino
+++ GPSLogger4/GPSLogger4/GPSLogger4.ino
@@ -21,6 +21,31 @@
     TX: 9 - 4 (TTL input)
 */
 
+/* flow
+  初期化ステップ1(シリアルポート、ソフトウェアシリアル、OLEDの初期化)
+  初期化ステップ2(GPSの設定：データの受信間隔・GPSより出力/ログするセンテンスの設定)
+  初期化ステップ3(SD・ファイルの初期化)
+  データを受信し、GPRMCもしくはGNRMCセンテンスのチェック→有効なRMCセンテンスならば日付を得る
+  日付、および既存ファイルと重複しないようファイル名を決定
+  ファイル作成日を指定していったんファイルをopen→close
+
+  loop
+  データ受信
+  LED点灯
+  ファイルが有効ならば
+    ファイルをopen
+    データをファイルに記録(シリアルに出力)
+    ファイルをclose
+    LED消灯
+  GPRMCもしくはGNRMCセンテンスのチェック、RMCセンテンスならば
+    有効なRMCセンテンスならば
+      validのindicator をOLEDに表示
+      緯度・経度・日付・時刻を得て、OLEDに情報を表示
+    でなければ
+      invalid dataであることの表示
+  loop end
+
+*/
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
@@ -28,6 +53,7 @@
 #include "U8glib.h"
 
 #define SD_CHIP_SELECT 10
+#define TimeZone (9)
 
 // initialize the library with the numbers of the interface pins
 SoftwareSerial gps(8, 9); // RX, TX
@@ -36,6 +62,10 @@ SoftwareSerial gps(8, 9); // RX, TX
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0); // I2C / TWI
 
 File logFile;
+
+int gpsHour, gpsMin, gpsSec;
+int gpsDay, gpsMonth, gpsYear;
+
 
 char filename[13];
 bool fileEnable = false;
@@ -64,6 +94,11 @@ bool checkSDFile()
     }
   }
   return false;
+}
+
+bool configure_GP8013T(int timeout) {
+
+  return true;
 }
 
 void setup() {
@@ -95,17 +130,11 @@ void setup() {
     u8g.setHiColorByRGB(255, 255, 255);
   }
   u8g.setFont(u8g_font_helvR10r);  //1.6KByte/17x22
- 
-  // initialize Software Serial and GT-720F
+
+  // initialize Software Serial and GPS
   gps.begin(9600); // ソフトウェアシリアルの初期化
-
-// configure output of GM-8013T
-
-//  send_nmea_command("PSRF103,00,00,2,01"); // GGA
-//  send_nmea_command("PSRF103,01,00,2,01"); // GLL
-//  send_nmea_command("PSRF103,02,00,2,01"); // GSA
-//  send_nmea_command("PSRF103,03,00,2,01"); // GSV
-//  send_nmea_command("PSRF103,04,00,2,01"); // RMC
+  // configure output of GM-8013T
+  configure_GP8013T(60);
   Serial.println("GPS ready");
 
   // initialize SD card
@@ -121,8 +150,13 @@ void setup() {
 void loop()
 {
   char str[90]; // string buffer
-  char *latitude, *longtude;
-
+  char latitude[12], longtude[11];
+  char *stat, *NS, *WE;
+  String utcTime, utcDate;
+  String s, s0;
+  int pos;
+  String localDate,localTime;
+  char localDate0[7],localTime0[7];
   if (fileEnable)
   {
     logFile = SD.open(filename, FILE_WRITE);
@@ -134,22 +168,58 @@ void loop()
     if (logFile) {
       logFile.println(str);
     }
-    if (strcmp(strtok(str, ","), "$GNRMC") == 0) { //if RMC line
-      strtok(NULL, ",");
-      strtok(NULL, ",");
-      latitude = strtok(NULL, ","); //get latitude
-      strtok(NULL, ",");
-      longtude = strtok(NULL, ","); //get longitude
-      strtok
- 
+    s = str;
+    pos = s.indexOf("$GNRMC");
+    if (pos >= 0) { // if RMC line
+//      strip_NMEA(s, &utcDate, pos, 9); strip date
+//      Serial.print("utcDate:");
+//      Serial.println(utcDate);
+      pos = strip_NMEA(s, &utcTime, pos, 1); // 1: utcTime
+      pos = strip_NMEA(s, &s0,     pos, 1); // 2: status
+      s0.toCharArray(stat, 1);
+      pos = strip_NMEA(s, &s0, pos, 1); // 3: latitude
+      s0.toCharArray(latitude, 11);
+      pos = strip_NMEA(s, &s0,     pos, 1); // 4: North/South
+      s0.toCharArray(NS, 1);
+      pos = strip_NMEA(s, &s0,     pos, 1); // 5: longitue
+      s0.toCharArray(longtude, 10);
+      pos = strip_NMEA(s, &s0,     pos, 1); // 6: West/East
+      s0.toCharArray(WE, 1);
+      pos = strip_NMEA(s, &utcDate, pos, 3); // 2: utcDate
+
+//      long l_utcTime = utcTime.toInt();
+//      long l_utcDate = utcDate.toInt();
+//      gpsDate(l_utcTime);
+//      gpsTime(l_utcDate);
+//      UCTtoLT();
+
       u8g.firstPage();
       do {
         u8g.setFont(u8g_font_unifont);
-        u8g.drawStr(0, 18,latitude);
-        u8g.drawStr(0, 36,longtude);
-      } while(u8g.nextPage());
+        localTime = utcTime;
+        localDate = utcDate;
+//        localTime=String(gpsHour)+String(gpsMin)+String(gpsSec);
+//        localDate=String(gpsYear)+String(gpsMonth)+String(gpsMonth);
+        utcTime.toCharArray(localTime0,7);
+        utcDate.toCharArray(localDate0,7);
 
+        byte ofs = u8g.drawStr(0, 18, localTime0);        
+        u8g.drawStr(ofs + 10, 18, localDate0);
+
+        if (*stat == 'A') {
+          u8g.drawStr(0, 36, NS);
+          u8g.drawStr(10, 36, latitude);
+          u8g.drawStr(0, 48, WE);
+          u8g.drawStr(10, 48, longtude);
+        }
+      } while (u8g.nextPage());
+
+      Serial.print(utcTime);
+      Serial.print(' ');
+      Serial.println(utcDate);
+      Serial.print("latitude: ");
       Serial.println(latitude);
+      Serial.print("longitude: ");
       Serial.println(longtude);
     }
     //    Serial.println(str);
@@ -174,6 +244,33 @@ void recvStr(char *buf)
   buf[i] = '\0';  // \0: end of string
 }
 
+int strip_NMEA(String s, String *message, int pos, int count)
+{
+  int pos0, pos1, pos0new, i = 0;
+  pos0 = pos;
+  pos1 = s.indexOf(",", pos0);
+  while (i < count) {
+    pos0 = pos1 + 1;
+    pos1 = s.indexOf(",", pos0);
+    i += 1;
+  }
+  *message = s.substring(pos0, pos1);  
+/*  
+   Serial.print(s);
+    Serial.print(";");
+    Serial.print(pos);
+    Serial.print (",");
+    Serial.print(count);
+    Serial.print(";");
+    Serial.print(pos0);
+    Serial.print(",");
+    Serial.print(pos1);
+    Serial.print(",");
+    Serial.println(*message);
+*/
+  return pos1;
+}
+
 // send NMEA command with checksum to gps (software serial)
 void send_nmea_command(const char *p)
 {
@@ -194,28 +291,64 @@ void send_nmea_command(const char *p)
   gps.println(checksum, HEX);
   gps.print("\n\r");
 }
-
 // UTC -> (gpsHour, gpsMin, gpsSec)
 void gpsTime(long UTC)
 {
-  gpsHour = int(UTC/10000);
-  gpsMin = int(UTC%10000/100);
-  gpsSec = UTC%100;
+  gpsHour = int(UTC / 10000);
+  gpsMin = int(UTC % 10000 / 100);
+  gpsSec = UTC % 100;
 }
-
-// Latitude/Longitude
-void gpsLatLong(int lat1, int lat2, int long1, int long2)
-{
+/*
+  // Latitude/Longitude
+  void gpsLatLong(int lat1, int lat2, int long1, int long2)
+  {
   gpsLat = int(lat1/100) + (lat1%100)/60.0 + float(lat2)/10000.0/60.0;
   gpsLong = int(long1/100) + (long1%100)/60.0 + float(long2)/10000.0/60.0;
-}
-
+  }
+*/
 // Date ->(gpsYear,gpsMonth,gpsYear)
 void gpsDate(long dateRead)
 {
-  gpsDay = int(dateRead/10000);
-  gpsMonth = int(dateRead%10000/100);
-  gpsYear = dateRead%100; //last 2 digits, e.g. 2013-> 13
-
+  gpsDay = int(dateRead / 10000);
+  gpsMonth = int(dateRead % 10000 / 100);
+  gpsYear = dateRead % 100; //last 2 digits, e.g. 2013-> 13
 }
 
+void UCTtoLT()
+{
+  int leap=(gpsYear%4)*4/gpsYear - (gpsYear%100)*100/gpsYear + (gpsYear%400)*400/gpsYear ;
+  int DaysAMonth[12] = {31, 28+leap, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  gpsHour +=TimeZone;
+  if (gpsHour < 0)
+  {
+    gpsHour += 24;
+    gpsDay -= 1;
+    if (gpsDay < 1)
+    {
+      if (gpsMonth == 1)
+      {
+        gpsMonth = 12;
+        gpsYear -= 1;
+      }
+      else
+      {
+        gpsMonth -= 1;
+      }
+      gpsDay = DaysAMonth[gpsMonth - 1];
+    }
+  }
+  if (gpsHour >= 24)
+  {
+    gpsHour -= 24;
+    gpsDay += 1;
+    if (gpsDay > DaysAMonth[gpsMonth - 1])
+    {
+      gpsDay = 1;
+      gpsMonth += 1;
+      if (gpsMonth > 12) {
+        gpsYear += 1;
+        gpsMonth = 1;
+      }
+    }
+  }
+}
